e925178bff1ac2bbcc1a94f674b4a40dcc4963e8
DylanSmithSE <dylansmith.se@gmail.com>
2022-08-26 16:21:09 -0700

    saving changes computer crashing

--- index.php
+++ index.php
@@ -12,23 +12,14 @@ include("./config/db.php");
             <i class="fa-solid fa-circle color-identifier"></i>
             <h3 class="workout-name">Push 1</h3>
             <div class="card-options flex-container">
+                <div class="options">
+                    <p>Options</p>
+                    <p class="options-delete">Delete</p>
+                </div>
                 <i class="fa-solid fa-ellipsis-vertical"></i>
             </div>
         </article>
-        <article class="flex-container workout-card">
-            <i class="fa-solid fa-circle color-identifier"></i>
-            <h3 class="workout-name">Push 2</h3>
-            <div class="card-options flex-container">
-                <i class="fa-solid fa-ellipsis-vertical"></i>
-            </div>
-        </article>
-        <article class="flex-container workout-card">
-            <i class="fa-solid fa-circle color-identifier"></i>
-            <h3 class="workout-name">Push 3</h3>
-            <div class="card-options flex-container">
-                <i class="fa-solid fa-ellipsis-vertical"></i>
-            </div>
-        </article>
+
     </main>
 </div>
 <?php include("./inc/navbar.php") ?>
